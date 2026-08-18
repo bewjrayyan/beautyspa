619f220ae9d62417e2b7fed111a616692e517d11
bewjrayyan <bewjd3fcon@gmail.com>
2026-08-18 13:53:24 +0800

    feat: move stamp cards section to top of order body

--- modules/Order/Resources/views/admin/orders/show.blade.php
+++ modules/Order/Resources/views/admin/orders/show.blade.php
@@ -26,14 +26,14 @@
         <div class="order-show__body">
             <div class="row">
                 <div class="col-lg-8">
+                    @if (app('modules')->isEnabled('Loyalty') && ! empty($orderStampData))
+                        @include('loyalty::admin.orders.partials.stamp_information', $orderStampData)
+                    @endif
+
                     @include('order::admin.orders.partials.order_and_account_information')
 
                     @include('order::admin.orders.partials.address_information')
                     @include('order::admin.orders.partials.items_ordered')
-
-                    @if (app('modules')->isEnabled('Loyalty') && ! empty($orderStampData))
-                        @include('loyalty::admin.orders.partials.stamp_information', $orderStampData)
-                    @endif
 
                     @if (! empty($treatmentBooking?->activities) && $treatmentBooking->activities->isNotEmpty())
                         <div class="order-show__section">
